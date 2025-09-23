a12b13254bb1c84fcfdfe7117721dd8c63eb90e7
Meganton <ameganton@gmail.com>
2025-09-23 16:22:39 +0200

    fix: add check for legal_value attribute before validation in Config class

--- src/mfpbench/config.py
+++ src/mfpbench/config.py
@@ -146,8 +146,9 @@ class Config(ABC, Mapping[str, Any]):
                     )
                 # No need to do anything here
             else:
-                if not hp.legal_value(v):
-                    raise ValueError(f"Value {v} is not legal for {k}")
+                if hasattr(hp, "legal_value"):
+                    if not hp.legal_value(v):
+                        raise ValueError(f"Value {v} is not legal for {k}")
                 hp.default_value = v
 
     @classmethod
